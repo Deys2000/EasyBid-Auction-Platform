a1386170a7d039ecc384931c0999e9833d96ce6b
Syed Ameen Quadri <ameenquadri2000@gmail.com>
2023-04-07 13:55:48 -0400

    react front end progress

--- ShipmentMicroservice/frontend/src/App.tsx
+++ ShipmentMicroservice/frontend/src/App.tsx
@@ -11,64 +11,64 @@ type Greeting = {
 function App() {
     return (
       <div className="App">
-        <header className="App-header">          
-          Shipment Interface      
-          <p>
-            Shipping Details <br/>
-            RECEIPT: First Name*, 
-            Last Name*, Street*, Number*, Province*, 
-            Country*, PostalCode*, Total Cost* <br/>             
-            <br/>          
-            <br/>
-            Your item will be shipped in XXX days<br/>
-            <BackToMainButton/> 
-          </p>          
+        <header className="App-header">                    
+<h1>Shipment Details Page</h1>
+<div>Your Reciepts</div>
+        <ListOfReciepts/>
+        <BackToMainButton/> 
+         
         </header>
       </div>
     );
   }
 
+  
+
 
 function BackToMainButton() {
   return (
     <a href="http://localhost:3002" target="_blank" rel="noopener noreferrer">
+      <hr/>
       <button> Back to Main Page </button>
     </a>
   );
 }
 
+function ListOfReciepts() {
+  
+  const [data, setData] = useState([]);
 
-// function App() {
-//   const [greeting, setGreeting] = useState<Greeting>();
-//   useEffect(() => {
-//     fetch("/api")
-//       .then(res => res.json())
-//       .then(setGreeting)
-//       .catch(console.error);
-//   }, [setGreeting]);
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         {greeting ? (
-//           <p>Hello from {greeting.name}</p>
-//         ) : (
-//           <p>Loading...</p>
-//         )}
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+  fetch('/read/all')
+  .then(response => response.json())
+  .then(data => {
+    setData(data);
+  })
+  .catch(error => {
+    //
+  });
+  
+  return (   
+    <div>
+      <hr/>
+      <h3>Receipt 1</h3>
+      <p>
+        {data}
+      </p>
+      <p> 
+        First Name <br/>
+        Last Name <br/>
+        Street <br/>
+        Number <br/>
+        Province <br/>
+        Country <br/>
+        Postal Code <br/>
+        Total Paid <br/>
+        Item ID <br/>
+      </p> 
+        <h5>Shipping Details</h5>        
+        This Item will be shipped in XXX days   
+    </div> 
+  );
+}
 
 export default App;
